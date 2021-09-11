b498692198342a729c964c9c39ec9d7722d444ba
sunqi <sunqi@xiao100.com>
2021-09-11 12:15:27 +0800

    feat: typescript

--- routes/users.ts
+++ routes/users.ts
@@ -1,12 +1,14 @@
-var express = require('express');
-var router = express.Router();
+import {RequestHandler} from 'express';
+
+const express = require('express');
+const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('users respond with a resource');
-});
+} as RequestHandler);
 router.get('/xxx', function (req, res, next) {
   res.send('xxx respond with a resource');
-});
+} as RequestHandler);
 
 module.exports = router;
